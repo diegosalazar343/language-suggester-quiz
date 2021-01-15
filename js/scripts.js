b9369a6f1a418da18fc31c266abf9b4038858e6f
diegosalazar343 <diegosalazar343@gmail.com>
2021-01-15 11:24:46 -0800

    add branching in user logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function results(input1, input2, input3, input4, input5) {
     programming = "Ruby";
   } else if(total <= 11) {
     programming = "C#";
-  } else if(total <=16){
+  } else if(total <= 16) {
     programming = "Python";
   } else {
     programming = "Swift";
@@ -26,14 +26,18 @@ $(document).ready(function(){
     const season = parseInt($("#season").val());
     const programming = results(superpower, disney, animal, language, season);
 
-    if(programming ===) {
-
-    } else if(programming ===){
-
-    } else if(programming === ){
-
+    if(programming === "Ruby") {
+      $("#ouput").show();
+      $("#answer").text("Ruby");
+    } else if(programming === "C#"){
+      $("#ouput").show();
+      $("#answer").text("C#");
+    } else if(programming === "Python"){
+      $("#ouput").show();
+      $("#answer").text("Python");
     } else {
-
+      $("#ouput").show();
+      $("#answer").text("Swift");
     }
   });
 });
